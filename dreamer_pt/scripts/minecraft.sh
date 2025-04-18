1892bb6c9850241f9fed1d01a46cddf1bfed9f7e
zinengtang <dokidokiyesdisco@gmail.com>
2025-04-18 07:05:40 +0000

    1

--- dreamer_pt/scripts/minecraft.sh
+++ dreamer_pt/scripts/minecraft.sh
@@ -1,1 +1,1 @@
-python3 dreamer.py --configs minecraft --task minecraft_diamond --logdir ./logdir/minecraft_diamond
+xvfb-run python3 dreamer.py --configs minecraft --task minecraft_diamond --logdir ./logdir/minecraft_diamond
